6d49dde863a0ead633f7ef9587fb41b73aa6769a
Bacchus Jackson <34331871+BacchusJackson@users.noreply.github.com>
2023-10-20 09:23:36 -0500

    feat: add optional profile and region flags

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ const help = `S3 Upload
 
 Upload a file to S3 using the AWS SDK.
 
+
 usage:
 	s3upload [OPTIONS] -f TARGET_FILE -k S3_KEY -b BUCKET
 
@@ -36,13 +37,25 @@ Required Flags:
 Optional Flags:
 	-h help Message and command usage
 	-q quiet Supress logging output
+	-p profile Share Profile, if not defined will default to AWS_PROFILE
+	-r region AWS Region, if not defined will default to AWS_REGION
 
 Additional Flags:
 	-e S3 BaseEndpoint Used for debugging purposes
-	
+
+Environment Variables:
+	AWS_PROFILE optional the profile identifier in the shared config
+	AWS_REGION optional the target region for the S3 bucket
+
 Example:
 
-	AWS_DEFAULT_PROFILE="my-profile" s3upload -f file-1.txt -k path/to/file/file-1.txt -b my-bucket
+	s3upload -f file-1.txt -k path/to/file/file-1.txt -b my-bucket
+
+Parameters order of precedence is as follows:
+
+1. Flag Options
+2. Environment Variables
+3. Profile Config Options, if supported
 `
 
 const ExitOK = 0
@@ -53,7 +66,10 @@ func main() {
 	fileFlag := flag.String("f", "", "Target file to upload")
 	keyFlag := flag.String("k", "", "Target Key for the file in S3")
 	bucketFlag := flag.String("b", "", "Target bucket for the upload")
-	endpoint := flag.String("e", "", "BaseEndpoint URL, used for debugging")
+	endpointFlag := flag.String("e", "", "BaseEndpoint URL, used for debugging")
+
+	profileFlag := flag.String("p", "", "Shared profile identifier")
+	regionFlag := flag.String("r", "", "The target region for the s3 bucket")
 
 	quietFlag := flag.Bool("q", false, "supressing logging output")
 	helpFlag := flag.Bool("h", false, "help message and command usage")
@@ -84,6 +100,16 @@ func main() {
 			flagErrs = errors.Join(flagErrs, err)
 		}
 	}
+	profile := os.Getenv("AWS_PROFILE")
+	region := os.Getenv("AWS_REGION")
+
+	if profileFlag != nil && strings.TrimSpace(*profileFlag) != "" {
+		profile = *profileFlag
+	}
+
+	if regionFlag != nil && strings.TrimSpace(*regionFlag) != "" {
+		region = *regionFlag
+	}
 
 	if flagErrs != nil {
 		os.Exit(ExitSystemFailure)
@@ -93,12 +119,15 @@ func main() {
 
 	// Load the AWS Configuration file from Shared Config
 	// https://docs.aws.amazon.com/sdkref/latest/guide/file-format.html
-	cfg, err := config.LoadDefaultConfig(context.Background())
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithSharedConfigProfile(profile))
 
 	if err != nil {
-		slog.Error("failed to load AWS default profile. Use AWS_DEFAULT_PROFILE", "err", err,
+		slog.Error("failed to load AWS default profile. Use AWS_PROFILE", "err", err,
 			"help_url", "https://docs.aws.amazon.com/sdkref/latest/guide/file-format.html")
 	}
+	cfg.Region = region
+
+	slog.Debug("config loaded", "region", cfg.Region, "profile", profile)
 
 	slog.Debug("open", "filename", *fileFlag)
 	f, err := os.Open(*fileFlag)
@@ -106,11 +135,11 @@ func main() {
 		slog.Error("failed open", "filename", *fileFlag, "err", err)
 		os.Exit(ExitSystemFailure)
 	}
-	if endpoint == nil || strings.TrimSpace(*endpoint) == "" {
-		endpoint = nil
+	if endpointFlag == nil || strings.TrimSpace(*endpointFlag) == "" {
+		endpointFlag = nil
 	}
 
-	err = s3Upload(cfg, *keyFlag, *bucketFlag, endpoint, f)
+	err = s3Upload(cfg, *keyFlag, *bucketFlag, endpointFlag, f)
 
 	if err != nil {
 		os.Exit(ExitAPIFailure)
